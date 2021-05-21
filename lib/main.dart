31acbce9f804211b23a7513d48b50fcb852d1417
BorisGautier <tchoukouahaboris@outlook.fr>
2021-05-21 17:11:21 +0100

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Gautier',
+      title: 'Boris Gautier Personal WebSite',
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: kPrimaryColor,
